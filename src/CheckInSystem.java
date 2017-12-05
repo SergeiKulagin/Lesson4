cbe631b07c9ebe8d3b8902eb83bd71702fc3c980
Sergei Kulagin <zosyastyles_gg@mail.ru>
2017-12-05 23:37:56 +0300

    что-то там такое сякое

--- src/CheckInSystem.java
+++ src/CheckInSystem.java
@@ -1,9 +1,35 @@
-import java.io.File;
+import java.io.*;
 
 public class CheckInSystem {
     private File checkInFile;
 
-    public void checkInSystem (int numbersTeam) {
-        RaceFeature myRacFeature = new RaceFeature();
+    public void readAFile(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        File newFile = new File("C:\\Users\\User\\Desktop\\myFile.txt");
+        try {
+            if (!newFile.exists()) {
+                newFile.createNewFile();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        FileWriter writer;
+        try {
+            writer = new FileWriter(newFile);
+            if (inputLine != null) {
+                writer.write(inputLine);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
