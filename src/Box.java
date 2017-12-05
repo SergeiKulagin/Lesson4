dba34d0691db5e35dc324f839336ddb3ab644038
Sergei Kulagin <zosyastyles_gg@mail.ru>
2017-12-05 22:30:14 +0300

    что-то там такое сякое

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+public class Box extends CarPark {
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    private Team team;
+
+}
