a8602116179a8c093ff52693b1b46610eeea59ad
Sergei Kulagin <zosyastyles_gg@mail.ru>
2017-12-05 23:33:34 +0300

    что-то там такое сякое

--- src/RaceFeature.java
+++ src/RaceFeature.java
@@ -1,18 +1,29 @@
+import java.io.File;
 import java.util.List;
 
 public class RaceFeature {
     public CarPark raceFeatureCarPark;
-    public List <Team> myTeam;
+    public List<Team> myTeam;
+
+    public RaceFeature() {
+    }
 
     public CarPark getRaceFeatureCarPark() {
+
         return raceFeatureCarPark;
     }
 
     public void setRaceFeatureCarPark(CarPark raceFeatureCarPark) {
+
         this.raceFeatureCarPark = raceFeatureCarPark;
     }
 
-    public RaceFeature() {
+    public void checkInCarPark(int numbersTeam) {        // регистрация на автостоянке гонок машин зрителей
+        String[] listNumbers = UserHelper.randomMassiveNumbers(numbersTeam);
+        for (int count = 0; count < numbersTeam; count++) {
+            AutoCar car = new AutoCar(listNumbers[count]);
+            raceFeatureCarPark.addCar(car);
+        }
 
     }
 }
