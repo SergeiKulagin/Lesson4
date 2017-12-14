b7a3f0f25a19727452e82b28933fb4da35d8eea9
Sergei Kulagin <zosyastyles_gg@mail.ru>
2017-12-14 23:34:34 +0300

    Часть3

--- src/CheckingSystem.java
+++ src/CheckingSystem.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckingSystem {
 
-    public static void registerCarsOfTeam () {
+    public static List<BoxesZone> registerCarsOfTeam(List<Car> teamCars) {
+        List <BoxesZone> teamBoxesZone = new ArrayList<>();
+        for (int i = 0; i < teamCars.size(); i=+3){
+            BoxesZone boxesZone = new BoxesZone();
+            boxesZone.addCar(teamCars.get(i));
+            boxesZone.addCar(teamCars.get(i+1));
+            boxesZone.addCar(teamCars.get(i+2));
+            teamBoxesZone.add(boxesZone);
+        }
+        return teamBoxesZone;
 
     }
-    public static void registerCarsOfGuests (Car car){
 
+    public static CarPark registerCarsOfGuests(List<Car> guestCars) {
+        CarPark guestCarPark = new CarPark();
+        for (int i = 0; i < guestCars.size(); i++) {
+            guestCarPark.addCar(guestCars.get(i));
+        }
+        return guestCarPark;
     }
 }
